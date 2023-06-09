7fd5dfe75280b6d93a022769fdc0488a83236605
Michel Tartas <85842887+micheltartas@users.noreply.github.com>
2023-06-09 01:45:24 -0300

    Navbar
    Adicionadas os Padrões de Navbar do projeto, além de diversos ícones.

--- resources/views/screens/home.blade.php
+++ resources/views/screens/home.blade.php
@@ -1,6 +1,3 @@
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-<link rel="stylesheet" href="/assets/css/home.css">
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -11,42 +8,62 @@
 
     <!-- CSS -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/navbar.css">
 
     <!-- JS -->
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
 
-<body>
-    <header></header>
-
-    <nav class="navbar navbar-dark border border-bottom-dark w-100 m-0 p-0">
-    <div class="d-flex flex-column w-100 justify-content-end bg-light p-0" id="navbarCollapse">
-
-        <div class="bg-dark">
-        <a id="navbar" class="navbar-brand d-flex align-items-left" href="#">
-            <img src="/assets/images/logo/logo.png" width="30" height="30" class="d-inline-block align-top p-" alt="Q">uestor
-        </a>
-        </div>
-
-        <div id="navbar" class="p-0 mt-0 mb-0 px-lg-0 bg-light w-100 mb-0 d-flex align-items-center">
-        <a class="nav-link" href="#">
-            <img src="/assets/images/ico/la-home.png" width="16" height="16" class="d-inline-block" alt=""><i class="text-black">- Início</i>
-        </a>
-        </div>
-    </div>
-    </nav>
-    
-    <main>
-        <div class="">Hi Home</div>
-        <div>
-            <ul>
-                <li><a href="{{ route('home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a></li>
-                <li><a href="{{ route('extratos') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Extratos</a></li>
-                <li><a href="{{ route('usuarios') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Usuários</a></li>
-                <li><a href="{{ route('clientes') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Clientes</a></li>
-                <li><a href="{{ route('capturas') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Status Captura</a></li>
+<body class="m-0">
+  <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-1 p-0 navbar-esquerda full-height justify-content-start align-items-center">
+            <ul class="nav flex-column my-0">
+                <li class="mt-4"><img src="./assets/images/ico/la-bars.png" width="24" height="24" class="d-inline-block" title="Menu" alt="Home"> </li>
+                <li class="mb-3 navbar-esquerda-segundo-li"><a href="{{ route('home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline"> <img src="./assets/images/ico/la-home.png" width="24" height="24" class="d-inline-block" title="Home" alt="Home"> </a></li>
+                <li class="mb-3 navbar-esquerda-outros-li"><a href="{{ route('extratos') }}" class="text-sm text-gray-700 dark:text-gray-500 underline"><img src="./assets/images/ico/la-th-list.png" width="24" height="24" class="d-inline-block" title="Extratos" alt="Extratos"></a></li>
+                <li class="mb-3 navbar-esquerda-outros-li"><a href="{{ route('usuarios') }}" class="text-sm text-gray-700 dark:text-gray-500 underline"> <img src="./assets/images/ico/la-user.png" width="24" height="24" class="d-inline-block" title="Usuários"  alt="Usuários"> </a></li>
+                <li class="mb-3 navbar-esquerda-outros-li"><a href="{{ route('clientes') }}" class="text-sm text-gray-700 dark:text-gray-500 underline"> <img src="./assets/images/ico/la-building.png" width="24" height="24" class="d-inline-block" title="Clientes"  alt="Clientes"> </a></li>
+                <li class="mb-3 navbar-esquerda-outros-li"><a href="{{ route('capturas') }}" class="text-sm text-gray-700 dark:text-gray-500 underline"> <img src="./assets/images/ico/la-th-list.png" width="24" height="24" class="d-inline-block" title="Capturas"  alt="Capturas"> </a></li>
             </ul>
         </div>
-    </main>
+
+      <div class="col">
+        <div class="row">
+          <div class="col-lg-12 m-0 p-0">
+            
+            <!-- Navbar vertical na parte superior direita -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-light p-0">
+              <div class="d-flex flex-column w-100 justify-content-end bg-light p-0" id="navbarCollapse">
+                
+                <div class="bg-dark">
+                  <a id="navbar" class="navbar-brand d-flex align-items-left" href="#">
+                    <img src="./assets/images/logo/logo.png" width="30" height="30" class="d-inline-block align-top p-" alt="Q"><i class="text-white">uestor</i>
+                  </a>
+                </div>
+                  
+                <div id="navbar" class="p-0 mt-0 mb-0 px-32 bg-light d-flex align-items-center">
+                  <a class="nav-link" href="#">
+                    <img src="./assets/images/ico/la-home.png" width="16" height="16" class="d-inline-block" alt="">
+                    <span class="ms-2 text-black"> Início</span>
+                  </a>
+                </div>              
+                
+              </div>
+            </nav>
+          </div>
+          <div class="col-lg-12">
+            <!-- Conteúdo do site -->
+            <div class="row">
+              <div class="col-lg-12">
+                <!-- Aqui você pode adicionar o conteúdo do site -->
+                Conteúdo Principal
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
