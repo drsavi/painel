77b03616798704ede6fbf2b2b1d20000bd65db4d
Michel Tartas <85842887+micheltartas@users.noreply.github.com>
2023-06-12 15:44:32 -0300

    Extratos Atualizada
    Atualizada a View base dos extratos.

--- resources/views/screens/clientes.blade.php
+++ resources/views/screens/clientes.blade.php
@@ -7,46 +7,44 @@
 @section('content')
 
 <body class="m-0">
-    <div class="teste">
-        <div class="container-fluid">
-            <div class="mx-4 mt-4 tabela-usuario">
-                <div class="ml-4 rounded border border-secondary">
-                    <div class="h3 p-3 border-bottom border-secondary">
-                        <strong>Clientes</strong>
-                    </div>
-
-                    <table class="table table-bordered table-striped">
-                        <thead class="">
-                        <tr>
-                            <th class="align-middle">Nome</th>
-                            <th class="align-middle">CNPJ</th>
-                            <th class="align-middle">ID Questor</th>
-                            <th class="align-middle">Status</th>
-                            <th class="align-middle col-lg-2"></th>
-                        </tr>
-                        </thead>
-
-                        <tbody>
-                        @if(isset($clientes))
-                            @foreach ($clientes as $cliente)
-                                <tr>
-                                    <td>{{ $cliente->nome }}</td>
-                                    <td>{{ $cliente->cnpj }}</td>
-                                    <td>{{ $cliente->idQuestor }}</td>
-                                    <td>{{ $cliente->status }}</td>
-                                    <td>
-                                        <a class="btn"><img src="./assets/images/ico/la-pen.png" width="16" height="16" class="d-inline-block" title="Editar" alt="Editar"></a>
-                                        <button class="btn" onclick="confirmDelete({{ $cliente->id }})"><img src="./assets/images/ico/la-trash.png" width="16" height="16" class="d-inline-block" title="Excluir" alt="Excluir"></button>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        @endif
-                        </tbody>
-                    </table>
-
+    <div class="container-fluid">
+        <div class="mx-4 mt-4 tabela-usuario">
+            <div class="ml-4 rounded border border-secondary">
+                <div class="h3 p-3 border-bottom border-secondary">
+                    <strong>Clientes</strong>
                 </div>
+
+                <table class="table table-bordered table-striped">
+                    <thead class="">
+                    <tr>
+                        <th class="align-middle">Nome</th>
+                        <th class="align-middle">CNPJ</th>
+                        <th class="align-middle">ID Questor</th>
+                        <th class="align-middle">Status</th>
+                        <th class="align-middle col-lg-2"></th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    @if(isset($clientes))
+                        @foreach ($clientes as $cliente)
+                            <tr>
+                                <td>{{ $cliente->nome }}</td>
+                                <td>{{ $cliente->cnpj }}</td>
+                                <td>{{ $cliente->idQuestor }}</td>
+                                <td>{{ $cliente->status }}</td>
+                                <td>
+                                    <a class="btn"><img src="./assets/images/ico/la-pen.png" width="16" height="16" class="d-inline-block" title="Editar" alt="Editar"></a>
+                                    <button class="btn" onclick="confirmDelete({{ $cliente->id }})"><img src="./assets/images/ico/la-trash.png" width="16" height="16" class="d-inline-block" title="Excluir" alt="Excluir"></button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endif
+                    </tbody>
+                </table>
+
             </div>
         </div>
     </div>
-    </body>
+</body>
 @endsection
--- resources/views/screens/extratos.blade.php
+++ resources/views/screens/extratos.blade.php
@@ -8,11 +8,54 @@
 
     <!-- CSS -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/extrato.css">
 
     <!-- JS -->
     <script src="{{ asset('js/app.js') }}"></script>
 </head>
-<body>
-<div>Hi, Extratos</div>
+
+@extends('layouts.estrutura')
+
+@section('breadcrumb') Extratos @endsection
+@section('content')
+
+<body class="m-0">
+    <div class="container-fluid">
+        <div class="mx-4 mt-4 tabela-usuario">
+            <div class="ml-4 rounded border border-secondary">
+                <div class="h3 p-3 border-bottom border-secondary">
+                    <strong>Extratos</strong>
+                </div>
+
+                <table class="table table-bordered table-striped">
+                    <thead class="">
+                    <tr>
+                        <th class="align-middle">Certidão</th>
+                        <th class="align-middle">Competência</th>
+                        <th class="align-middle">Quantidade de CND</th>
+                        <th class="align-middle">Quantidade de CNPJ</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    @if(isset($clientes))
+                        @foreach ($clientes as $cliente)
+                            <tr>
+                                <td>{{ $cliente->nome }}</td>
+                                <td>{{ $cliente->cnpj }}</td>
+                                <td>{{ $cliente->idQuestor }}</td>
+                                <td>{{ $cliente->status }}</td>
+                            </tr>
+                        @endforeach
+                    @endif
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+
+@endsection
