5b705545c2199a42f13f5506c74148da7e5c380e
Michel Tartas <85842887+micheltartas@users.noreply.github.com>
2023-06-13 20:42:43 -0300

    Home
    Atualizada a Home para que fica de acordo com os requisitos.

--- resources/views/screens/home.blade.php
+++ resources/views/screens/home.blade.php
@@ -16,27 +16,11 @@
 
 @extends('layouts.estrutura')
 
-@section('breadcrumb') Principal @endsection
+@section('breadcrumb') Início @endsection
 @section('content')
 
 <body class="m-0">
-    <div class="container-fluid">
-        <div class="mx-4 mt-4 mb-4 tabela-usuario">
-            <div class="ml-4">
-                <div class="card p-4">
-                  <div class="card-header h3">
-                    <strong>Principal</strong>
-                  </div>
 
-                  <div class="card-body">
-                    <h5 class="card-title">Título do Corpo</h5>
-                    <p class="card-text">Conteúdo do Corpo do Card.</p>
-                  </div>
-
-                </div>
-            </div>
-        </div>
-    </div>
 </body>
 </html>
 
