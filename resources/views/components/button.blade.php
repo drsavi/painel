81e2cceffeb94ea5e4967fbbd639d60c2087a185
Debora <deb.rar.savi@gmail.com>
2023-06-13 20:20:38 -0300

    Componente botão

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,6 @@
+<button type="{{ $type }}" class="btn ml-auto" onclick="{{ $onClick }}">
+    <p class="h5 d-inline">
+        <img src="{{ $image }}" width="20" height="20" class="d-inline-block" title="{{ $title }}" alt="{{ $alt }}">
+        {{ $label }}
+    </p>
+</button>
