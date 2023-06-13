f0c47fef0db7e4a5e0a1fa5961a1fa62fd30558e
Debora <deb.rar.savi@gmail.com>
2023-06-13 17:24:58 -0300

    alterados campos migrations usuarios_clientes

--- database/migrations/2023_06_12_203820_usuarios_clientes.php
+++ database/migrations/2023_06_12_203820_usuarios_clientes.php
@@ -15,10 +15,10 @@ class UsuariosClientes extends Migration
     {
         Schema::create('usuarios_clientes', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('id_usuario');
-            $table->foreign('id_usuario')->references('id')->on('usuarios');
-            $table->unsignedBigInteger('id_cliente');
-            $table->foreign('id_cliente')->references('id')->on('clientes');
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes');
             $table->timestamps();
         });
     }
